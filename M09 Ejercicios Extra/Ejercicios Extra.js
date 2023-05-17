c1ead8f4398293528da2dde64711f2811c2e71bb
Auliorp <aulioroveroparedes@gmail.com>
2023-05-17 17:03:04 -0400

    M09 finalizado

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,7 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+  return Object.entries(objeto);
 }
 
 function numberOfCharacters(string) {
@@ -85,7 +86,15 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
-   
+   let nuevoArreglo = [];
+   for (let i = 0; i < array1.length; i++){
+      for (let j = 0; j < array2.length; j++){
+         if(array1[i] === array2[j]){
+            nuevoArreglo.push(array1[i]);
+         }
+      }
+   }
+   return nuevoArreglo;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
